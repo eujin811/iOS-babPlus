f4d9584f185cecddc57fec38d393324cc3bb4026
hongdonghyun <hong4367@gmail.com>
2020-02-06 11:11:06 +0900

    modified: BranchDetailViewController

--- babPlus/babPlus/Controller/BranchDetailViewController.swift
+++ babPlus/babPlus/Controller/BranchDetailViewController.swift
@@ -7,27 +7,86 @@
 //
 
 import UIKit
+import MapKit
 
 class BranchDetailViewController: UIViewController {
     
     private let backButtonItem = UINavigationItem()
-
+    private let mapView = MKMapView()
+    private let mapContainerView = UIView()
+    private let menuTableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tabBarItem.
+        view.backgroundColor = .white
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "lessthan"), style: .plain, target: self, action: #selector(didTapBackButtonItem(_:)))
-    }
-    
-    private func setupUI() {
-        
-    }
-    private func attribute() {
-        
+        setupUI()
     }
     
     @objc private func didTapBackButtonItem(_ sender : Any) {
         dismiss(animated: true)
     }
+    
+}
 
+extension BranchDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+// MARK: - Setup UI
+extension BranchDetailViewController {
+    
+    private func setupMapView() {
+        let safeArea = view.safeAreaLayoutGuide
+        let mapSize = self.view.frame.height * 0.3
+        mapContainerView.addSubview(mapView)
+        view.addSubview(mapContainerView)
+        
+        mapContainerView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2DMake(mapCenterlat, mapCenterlon), span: span)
+        mapView.setRegion(region, animated: true)
+    
+        NSLayoutConstraint.activate([
+            mapContainerView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            mapContainerView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            mapContainerView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            mapContainerView.heightAnchor.constraint(equalToConstant: mapSize),
+            
+            mapView.centerXAnchor.constraint(equalTo: mapContainerView.centerXAnchor),
+            mapView.centerYAnchor.constraint(equalTo: mapContainerView.centerYAnchor),
+            mapView.widthAnchor.constraint(equalToConstant: mapSize * 1.2),
+            mapView.heightAnchor.constraint(equalToConstant: mapSize * 0.9),
+        ])
+    }
+    
+    private func setupTableView() {
+        let safeArea = view.safeAreaLayoutGuide
+        [menuTableView].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            menuTableView.topAnchor.constraint(equalTo: mapContainerView.bottomAnchor),
+            menuTableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            menuTableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            menuTableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+        
+    }
+    
+    private func setupUI() {
+        setupMapView()
+        setupTableView()
+        menuTableView.dataSource = self
+    }
 }
